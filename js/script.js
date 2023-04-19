c66a7ccb44c0a8f75f16739b0a98443cdcb17a51
Reny Reyes <98413865+Reny8@users.noreply.github.com>
2023-04-19 16:37:40 -0400

    Add functionality to toggle menu view

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const input = document.getElementById('link-input');
 const form = document.getElementById('link-form');
 const errMsg = document.getElementById('err-msg');
+const hamburgerMenu = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
 
 form.addEventListener('submit', formSubmit);
+hamburgerMenu.addEventListener('click', navToggle);
 
 function formSubmit(event) {
   event.preventDefault();
@@ -14,4 +17,10 @@ function formSubmit(event) {
     input.classList.remove('border-red');
     alert('Successfully Added (Not really)');
   }
+}
+
+function navToggle() {
+  hamburgerMenu.classList.toggle('open');
+  menu.classList.toggle('hidden');
+  menu.classList.toggle('flex');
 }
